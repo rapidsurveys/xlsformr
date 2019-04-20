212ad507961ae5f00dd19abe130c47b538cf93c9
Ernest Guevarra <ernest@validmeasures.org>
2019-04-20 15:41:19 +0100

    create createHexSticker.R

--- inst/createHexSticker.R
+++ inst/createHexSticker.R
@@ -1,7 +1,7 @@
 library(hexSticker)
 
-sticker("inst/figures/roulette.png",
-        s_x = 1, s_y = 0.8, s_width = 0.7,
-        package = "bbw", p_color = "#E3BB00", p_y = 1.6,
-        h_size = 1.5, h_color = "#E3BB00", h_fill = NA,
-        filename = "man/figures/bbw.png")
+sticker("inst/figures/xls.png",
+        s_x = 1, s_y = 0.85, s_width = 0.35,
+        package = "xlsformr", p_color = "#800080", p_y = 1.5,
+        h_size = 1.5, h_color = "#800080", h_fill = NA,
+        filename = "man/figures/xlsformr.png")
